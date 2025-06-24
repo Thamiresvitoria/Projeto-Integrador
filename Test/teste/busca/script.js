73913df1171e35f3348f63b32701383410b38020
tvitoria <thamiresvitoriamunizdasilva20@gmail.com>
2025-06-24 01:20:44 -0300

    ajuste

--- Test/teste/busca/script.js
+++
@@ -1,8 +0,0 @@
-const campoBusca = document.getElementById('busca');
-const resultado = document.getElementById('resultado');
-
-// Mostra em tempo real o que está sendo digitado
-campoBusca.addEventListener('input', () => {
-  const texto = campoBusca.value.trim();
-  resultado.textContent = texto ? `Você está buscando por: "${texto}"` : '';
-});
